80002865d8cfd84cacbf63115ac1815553321abd
isaac-asdf <97187398+isaac-asdf@users.noreply.github.com>
2024-03-31 20:53:35 -0500

    displaying something...

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,150 @@
 #![no_std]
 #![no_main]
 
-use cyw43_pio::PioSpi;
+use core::cell::RefCell;
+
 use defmt::*;
+// use display_interface_spi::{SPIInterface, SPIInterfaceNoCS};
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
+use embassy_rp::spi;
+use embassy_rp::spi::{Blocking, Spi};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::blocking_mutex::Mutex;
+use embassy_time::{Delay, Duration, Timer};
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::Rectangle;
+// use embedded_graphics::primitives::PrimitiveStyleBuilder;
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    // prelude::*,
+    primitives::{Circle, Primitive, PrimitiveStyle, Triangle},
+};
+use mipidsi::options::{Orientation, Rotation};
 use {defmt_rtt as _, panic_probe as _};
 
-bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-});
+// Provides the Display builder
+use mipidsi::Builder;
 
-#[embassy_executor::task]
-async fn wifi_task(
-    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
-) -> ! {
-    runner.run().await
-}
+use crate::touch::Touch;
+
+const DISPLAY_FREQ: u32 = 64_000_000;
+const TOUCH_FREQ: u32 = 200_000;
+
+mod touch;
+mod waveshare35;
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
-    let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    info!("Hello World!");
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download 43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    let bl = p.PIN_13;
+    let rst = p.PIN_15;
+    let display_cs = p.PIN_9;
+    let dcx = p.PIN_8;
+    let miso = p.PIN_12;
+    let mosi = p.PIN_11;
+    let clk = p.PIN_10;
+    let touch_cs = p.PIN_16;
+    //let touch_irq = p.PIN_17;
 
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(
-        &mut pio.common,
-        pio.sm0,
-        pio.irq0,
-        cs,
-        p.PIN_24,
-        p.PIN_29,
-        p.DMA_CH0,
+    // create SPI
+    let mut display_config = spi::Config::default();
+    display_config.frequency = DISPLAY_FREQ;
+    display_config.phase = spi::Phase::CaptureOnSecondTransition;
+    display_config.polarity = spi::Polarity::IdleHigh;
+    let mut touch_config = spi::Config::default();
+    touch_config.frequency = TOUCH_FREQ;
+    touch_config.phase = spi::Phase::CaptureOnSecondTransition;
+    touch_config.polarity = spi::Polarity::IdleHigh;
+
+    let spi: Spi<'_, _, Blocking> =
+        Spi::new_blocking(p.SPI1, clk, mosi, miso, touch_config.clone());
+    let spi_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi));
+
+    let display_spi = SpiDeviceWithConfig::new(
+        &spi_bus,
+        Output::new(display_cs, Level::High),
+        display_config,
     );
+    let touch_spi =
+        SpiDeviceWithConfig::new(&spi_bus, Output::new(touch_cs, Level::High), touch_config);
 
-    static STATE: StaticCell<cyw43::State> = StaticCell::new();
-    let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-    unwrap!(spawner.spawn(wifi_task(runner)));
+    let mut touch = Touch::new(touch_spi);
 
-    control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
+    let dcx = Output::new(dcx, Level::Low);
+    let rst = Output::new(rst, Level::Low);
+    // dcx: 0 = command, 1 = data
 
-    let delay = Duration::from_secs(1);
+    // Enable LCD backlight
+    let mut bl = Output::new(bl, Level::High);
+    bl.set_high();
+
+    // display interface abstraction from SPI and DC
+    // let di = SPIDeviceInterface::new(display_spi, dcx);
+    let di = waveshare35::SPIDeviceInterface::new(display_spi, dcx);
+
+    // Define the display from the display interface and initialize it
+    let mut display = Builder::new(mipidsi::models::ILI9488Rgb565, di)
+        .reset_pin(rst)
+        .init(&mut Delay)
+        .unwrap();
+
+    // set default orientation
+    display.set_orientation(Orientation::new().rotate(Rotation::Deg90));
+
+    // Make the display all black
+    display.clear(Rgb565::BLACK).unwrap();
+    let area: Rectangle = Rectangle::new(Point::new(0, 0), Size::new(200, 100));
+    display.fill_solid(&area, Rgb565::BLUE);
+
+    // Draw a smiley face with white eyes and a red mouth
+    // draw_smiley(&mut display).unwrap();
+
     loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
+        // if let Some((x, y)) = touch.read() {
+        //     let style = PrimitiveStyleBuilder::new()
+        //         .fill_color(Rgb565::BLUE)
+        //         .build();
 
-        info!("led off!");
-        control.gpio_set(0, false).await;
-        Timer::after(delay).await;
+        // Rectangle::new(Point::new(x - 1, y - 1), Size::new(3, 3))
+        //     .into_styled(style)
+        //     .draw(&mut display)
+        //     .unwrap();
+        // }
     }
+}
+
+fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::Error> {
+    // Draw the left eye as a circle located at (50, 100), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 100), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw the right eye as a circle located at (50, 200), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 200), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw an upside down red triangle to represent a smiling mouth
+    Triangle::new(
+        Point::new(130, 140),
+        Point::new(130, 200),
+        Point::new(160, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
+    .draw(display)?;
+
+    // Cover the top part of the mouth with a black triangle so it looks closed instead of open
+    Triangle::new(
+        Point::new(130, 150),
+        Point::new(130, 190),
+        Point::new(150, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
+    .draw(display)?;
+
+    Ok(())
 }
---
+++ src/touch.rs
@@ -0,0 +1,59 @@
+/// Driver for the XPT2046 resistive touchscreen sensor
+use embedded_hal_1::spi::{Operation, SpiDevice};
+
+struct Calibration {
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
+    sx: i32,
+    sy: i32,
+}
+
+const CALIBRATION: Calibration = Calibration {
+    x1: 3880,
+    x2: 340,
+    y1: 262,
+    y2: 3850,
+    sx: 320,
+    sy: 240,
+};
+
+pub struct Touch<SPI: SpiDevice> {
+    spi: SPI,
+}
+
+impl<SPI> Touch<SPI>
+where
+    SPI: SpiDevice,
+{
+    pub fn new(spi: SPI) -> Self {
+        Self { spi }
+    }
+
+    pub fn read(&mut self) -> Option<(i32, i32)> {
+        let mut x = [0; 2];
+        let mut y = [0; 2];
+        self.spi
+            .transaction(&mut [
+                Operation::Write(&[0x90]),
+                Operation::Read(&mut x),
+                Operation::Write(&[0xd0]),
+                Operation::Read(&mut y),
+            ])
+            .unwrap();
+
+        let x = (u16::from_be_bytes(x) >> 3) as i32;
+        let y = (u16::from_be_bytes(y) >> 3) as i32;
+
+        let cal = &CALIBRATION;
+
+        let x = ((x - cal.x1) * cal.sx / (cal.x2 - cal.x1)).clamp(0, cal.sx);
+        let y = ((y - cal.y1) * cal.sy / (cal.y2 - cal.y1)).clamp(0, cal.sy);
+        if x == 0 && y == 0 {
+            None
+        } else {
+            Some((x, y))
+        }
+    }
+}
